6fafdd1575b1c99357401fad08049e644b6efe9e
Sharypov Konstantin <shariktlt@gmail.com>
2022-05-27 09:55:20 +0300

    Example aggregate sturcture for local build

---
+++ src/main/java/org/example/app/Main.java
@@ -0,0 +1,10 @@
+package org.example.app;
+
+import org.example.lib.MyLibClass;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLibClass lib = new MyLibClass();
+        lib.myLibMethod();
+    }
+}
